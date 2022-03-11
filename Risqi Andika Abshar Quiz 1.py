85adb2f3487a9b52982479aea0c6b45e315b7328
risqi23 <dikaabshar@gmail.com>
2022-03-11 22:43:51 +0700

    Quiz

---
+++ Risqi Andika Abshar Quiz 1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Python OOP
+
+# 1. OOP adalah program berorientasi objek, atau paradigma untuk meneraplan objek diskitar kedalam sebuah kode
+
+# In[ ]:
+
+
+def__init__(nama, umur alamat):
+    self.nama= nama
+    self.umur= umur
+    sel.alamat= alamat
+dika = nama("dika", 20, bojong)
+alvin = nama("alvin" 21, tegal)
+
+
+# In[2]:
+
+
+def get_oldest_age(*args):
+    return max(args)
+print("Yang paling tua diantara mereka  adalah: years old." format(
+    get_oldest_age(dika.age, alvin.age,())
+
+
+# 4. Custom exception handling adalah pengecualian untuk menangani sesuatu apabila terjadi eror
+
+# Artificial Intellegence
+
+# 1. Data terstruktur merupakan data yang disimpan diproses dan diambil dengan format tretap, sedangkan data tidak terstruktur adlah data yang abstrak atau tidak memiliki struktur khusus
+
+# 2. Perbedaanya adalah AI merupakan algoritma yang digunakan untuk dapat membangunya, Machine Learning adalah algoritma dengan data untuk membangunya, sedangkan deep learning fitur mesin otomatis contohnya seperti feature extraction
+
+# 3.Tradiosional programing mengetahui dulu background knowledge kemudian baru dibuat program sesuai fungsi yang diketahui
+#     sedangkan machine learning mengolah data dari masa lalu untuk memprediksi pola
+
+# 4. prediksi hasil panen, menejemen resiko, pencegahan hama
+# sedengkan di smart city itu ada CLM aplikasi uji resiko gejala korona
+# 
+
+# 
+# Math For AI
+
+# 1.Karena Matematika merupakan dasar dari teknologi kecerdasan buatan, komputer hanya mampu membaca bahasa dalam bentuk algoritma atau angka
+
+# 2.Vektor adalah besaran yang merepresentasikan dengan nilai dan arah, sedangkan skalar adalah besaran yang merepresentasikan nilai saja
+
+# 3.Euclidean metrik adalah jarak antara dua titik yang satu dengan yang lain yang dapat diukur dengna menggunakan penggaris, pada perhitungan jarak Euclidean digunakan formula Pythagoras
+
+# 4.Bagian inti dari AI adalah ML, ML perlu memprediksi beberapa output dalam pemrosesan apapun dan itu didasarkan pada matematika
+
+# Machine Learning
+
+# 1.Supervised learning Machine Learning model yang mempelajari data dengan label atau target. Sedangkan Unsupervised Learning adalah Machine Learning Model yang mempelajari pola data tanpa adanya target data. Contohnya klasifikasi regresi
+
+# 2.a, Supervised Learning, Metode supervised learning dilakukan dengan pemberian label pada dataset  yang digunakan oleh machine learning dan diklasifikasikan oleh pengembang dengan memungkinkan algoritma melihat tingkat akurasi kinerjanya
+#  b,unsupervised Learning etode semi-supervised learning bisa disebut juga sebagai metode machine learning tanpa pengawasan. Sehingga, prosesnya dilakukan pada dataset mentah yang tidak berlabel dan algoritma machine learning akan mencoba mengidentifikasi pola dan relasi antar data tanpa bantuan dari pengembang., 
+#  c,reinforcement learning Metode machine learning yang satu ini dijalankan dengan menggunakan dataset bersistem “rewards/punishment” dan menawarkan umpan balik ke algoritma untuk belajar dari pengalamannya secara random.
+
+# 3.Regresi adalah suatu teknik analisis untuk mengidentifikasi relasi atau hubungan diantara dua variabel atau lebih sedangkan Pada dasarnya, ada dua tipe model regresi yaitu simple regression (regresi sederhana), dan multiple regression (regresi berganda. 
+
+# 4.ANN atau Jaringan Saraf Tiruan adalah upaya untuk mensimulasikan jaringan neuron yang membentuk otak manusia sehingga komputer akan dapat mempelajari berbagai hal dan membuat keputusan dengan cara yang mirip manusia. fungsinya mengklasifikasikan informasi, memprediksi hasil dan data cluster
